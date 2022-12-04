b006132d87a69bbef52fbe2c88f0f558ced89cf0
MiguelBarreraDev <barreram987@gmail.com>
2022-12-04 15:47:33 -0500

    Update: Propiedades del schema pacientes

--- backend/database/migrations/2022_12_04_202526_create_pacientes_table.php
+++ backend/database/migrations/2022_12_04_202526_create_pacientes_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('pacientes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('lastname');
             $table->timestamps();
         });
     }
