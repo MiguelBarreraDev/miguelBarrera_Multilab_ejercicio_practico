621e9409aeb708099ca9a6afe987a828e585e872
MiguelBarreraDev <barreram987@gmail.com>
2022-12-05 00:33:32 -0500

    Add: Login controller

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\LoginRequest;
 
 class LoginController extends Controller
 {
-    //
-}
+    public function show () {
+        return view('auth.login');
+    }
+
+    public function login (LoginRequest $request) {
+        $credentials = $request->getCredentials();
+        if (!Auth::validate($credentials)) {
+            return redirect('/login')->withErrors('Por favor verifique su correo y contraseña');
+        }
+    
+        $user = Auth::getProvider()->retrieveByCredentials($credentials);
+        Auth::login($user);
+
+        return redirect('/home');
+    }
+ }
