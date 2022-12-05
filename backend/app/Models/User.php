83c57ee6efe15c90d8fc43f5b7e5717229f5222e
MiguelBarreraDev <barreram987@gmail.com>
2022-12-04 23:57:01 -0500

    Add: Encriptación de contraseña

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Encripta la contraseña.
+     *
+     * @param string $value
+     */
+    public function setPasswordAttribute ($value) {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
