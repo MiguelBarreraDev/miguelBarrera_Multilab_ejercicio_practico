a18b5bb101882bc602c43bf72d7c51ba7192271a
MiguelBarreraDev <barreram987@gmail.com>
2022-12-05 03:58:33 -0500

    Update: Extend from layout.app

--- backend/resources/views/home/home.blade.php
+++ backend/resources/views/home/home.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Welcome to Home
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="title is-3 has-text-primary has-text-centered">Welcome {{ auth()->user()->name }}</h1>
+@endsection
