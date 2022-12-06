7c1e618bd7b87431715748343e0077d5d205d1d2
MiguelBarreraDev <barreram987@gmail.com>
2022-12-06 03:01:15 -0500

    Update: Renamt with Pascal Case format

--- backend/app/Models/OrderDetail.php
+++ backend/app/Models/OrderDetail.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Medical_test extends Model
+class OrderDetail extends Model
 {
     use HasFactory;
 }
