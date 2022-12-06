21ebcc480b421f0bc4c59f6f47de224c6f3bf0c0
MiguelBarreraDev <barreram987@gmail.com>
2022-12-06 01:24:43 -0500

    Update: Rules name is required

--- backend/app/Http/Requests/SignupRequest.php
+++ backend/app/Http/Requests/SignupRequest.php
@@ -24,6 +24,7 @@ class SignupRequest extends FormRequest
     public function rules()
     {
         return [
+            'name' => 'required',
             'email' => 'required',
             'password' => 'required'
         ];
