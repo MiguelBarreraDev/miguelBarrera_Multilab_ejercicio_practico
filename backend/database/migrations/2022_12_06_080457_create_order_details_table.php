d8d0496ac11edec7e5e311bf40029b84213ed4e3
MiguelBarreraDev <barreram987@gmail.com>
2022-12-06 03:25:27 -0500

    Add: price field in order_details table

--- backend/database/migrations/2022_12_06_080457_create_order_details_table.php
+++ backend/database/migrations/2022_12_06_080457_create_order_details_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('order_details', function (Blueprint $table) {
             $table->id();
+            $table->float('price', 7,2);
             $table->timestamps();
         });
     }
