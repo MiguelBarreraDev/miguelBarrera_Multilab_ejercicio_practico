9461f2cd695bf8fcc99b11ec5a96f10c16a3867a
MiguelBarreraDev <barreram987@gmail.com>
2022-12-08 02:46:15 -0500

    Update: Formated code

--- backend/resources/js/addOrderForm.js
+++ backend/resources/js/addOrderForm.js
@@ -1,12 +1,8 @@
-const addOrderForm = document.getElementById('add-order-form');
-const addOrderSubmitButton = document.getElementById('add-order-submit-button');
+const addOrderForm = document.getElementById("add-order-form");
+const addOrderSubmitButton = document.getElementById("add-order-submit-button");
 
-console.log('hola')
-console.log({addOrderForm})
-console.log({addOrderSubmitButton})
-
-addOrderForm?.addEventListener('submit', () => {
+addOrderForm?.addEventListener("submit", () => {
     if (addOrderForm?.checkValidity()) {
-        addOrderSubmitButton?.setAttribute('disabled', true);
+        addOrderSubmitButton?.setAttribute("disabled", true);
     }
-})
+});
